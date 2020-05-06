dfce932175cc411170dd7314fb836c924ffae40b
usamajawed <usamajawed573@gmail.com>
2020-05-06 16:14:49 +0500

    Created Main Components
    Created topbar, sidebar and app body component files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './common/topbar/topbar.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { BodyComponent } from './common/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    SidebarComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
